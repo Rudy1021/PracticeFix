642963df29c88832aa421db4b20a3a7a4df09b9c
bonny890322 <71811787+bonny890322@users.noreply.github.com>
2021-04-08 13:16:59 +0800

    Add files via upload

---
+++ PracticeFix/CreatePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PracticeFix
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CreatePage : ContentPage
+    {
+        public CreatePage()
+        {
+            InitializeComponent();
+        }
+        void canael_click(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+        void confirm_click(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
